425568e12e24d64e012e79d4ef9f4cad9825b292
Javani <moon.blade4@gmail.com>
2022-11-01 18:03:09 +0600

    add slick slider and style

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-    $('.carousel__inner').slick();
+    $('.carousel__inner').slick({
+        dots: true,
+        slidesToShow: 1,
+        speed: 1200
+    });
   });
