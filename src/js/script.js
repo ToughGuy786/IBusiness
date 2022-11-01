a3046315e358d55d8ba21512cd39da484118bd6a
Javani <moon.blade4@gmail.com>
2022-11-01 16:24:41 +0600

    slick

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,3 @@
-const slider = tns({
-    container: '.carousel__inner',
-    items: 1,
-    slideBy: 'page',
-    autoplay: false,
-    controls: false,
-    nav: false
-});
-
-document.querySelector('.prev').addEventListener('click', function ()  {
-    slider.goTo('prev');
-});
-
-document.querySelector('.next').addEventListener('click', function ()  {
-    slider.goTo('next');
-});
-
-
-
-
+$(document).ready(function(){
+    $('.carousel__inner').slick();
+  });
