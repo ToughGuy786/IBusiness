f39759e852c899e469768dd6ee59a978ce50451a
Javani <moon.blade4@gmail.com>
2022-11-03 19:40:58 +0600

    ...

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         dots: true,
         slidesToShow: 1,
-        speed: 1200
+        speed: 1200,
+        prevArrow: false,
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/ic_arrowright.svg"></button>'
     });
   });
