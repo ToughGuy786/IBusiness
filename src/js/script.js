4e3c802b9625078ed75be62047bbbca473e324cd
Javani <moon.blade4@gmail.com>
2022-11-15 19:39:23 +0600

    create new branch

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,22 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.carousel__inner').slick({
-        dots: true,
+        dots: false,
         slidesToShow: 1,
         speed: 1200,
         prevArrow: false,
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/ic_arrowright.svg"></button>',
+        adaptiveHeight: true,
+        nextArrow: '<button type="button" class="slick-next"><img src="./icons/ic_arrowright.svg"></button>',
     });
-  });
+
+    if (/Android/i.test(navigator.userAgent)) {
+        console.log("Вы используете android");
+        $('.promo_link').attr('href', 'https://play.google.com/store/apps/details?id=ibusiness.asia');
+    } else if (/iPhone|iPad|iPod/i.test(navigator.userAgent)) {
+        console.log("Вы используете apple");
+        console.log(navigator.userAgent);
+    } else {
+        console.log("Вы используете ПК.");
+        $('.promo_link').attr('href', 'https://app.mypage.kz/login');
+    }
+
+});
